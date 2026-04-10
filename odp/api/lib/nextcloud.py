5b0a060d70ad6f5662f39443f471bdd3a5f79817
dylan <d.pivo@saeon.nrf.ac.za>
2026-04-10 08:43:47 +0200

    Resolve conflict changes

--- odp/api/lib/nextcloud.py
+++ odp/api/lib/nextcloud.py
@@ -55,7 +55,6 @@ def delete_folder_from_nextcloud(folder_name):
 def _clear_and_create_folder(folder_name):
     """Deletes the folder and then re-creates it."""
     folder_url = f"{config.NEXTCLOUD.URL}{NEXTCLOUD_FOLDER}{folder_name}/"
-    auth = (config.NEXTCLOUD.USER, config.NEXTCLOUD.PASSWORD)
 
     delete_folder_from_nextcloud(folder_name)
 
@@ -64,4 +63,3 @@ def _clear_and_create_folder(folder_name):
         folder_url,
         auth=(config.NEXTCLOUD.USER, config.NEXTCLOUD.PASSWORD)
     )
-
